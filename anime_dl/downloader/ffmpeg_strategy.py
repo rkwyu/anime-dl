282d5667e05cd88311663af8ae38bc3c21a0c087
abefan2019 <abefan2019@gmail.com>
2025-09-13 17:43:16 +0800

    Update ffmpeg_strategy.py

--- anime_dl/downloader/ffmpeg_strategy.py
+++ anime_dl/downloader/ffmpeg_strategy.py
@@ -28,12 +28,27 @@ class FfmpegStrategy(Strategy):
                 config_loader.get(section="DIRECTORY", key="output"),
                 sanitize_filename(fnt),
             )
+            vtt = os.path.join(
+                config_loader.get(section="DIRECTORY", key="output"),
+                "vtt",
+                f"{episode.episode_name}.vtt",
+            )
             os.makedirs(os.path.dirname(output), exist_ok=True)
             logger.info(f"started download: {filename} ({url})")
             stream = ffmpeg.input(url)
             stream = ffmpeg.output(stream, output_t, vcodec="copy", acodec="copy")
             ffmpeg.run(stream)
-            os.rename(output_t,output)
+            if os.path.exists(vtt):
+                ffmpeg.output(
+                    ffmpeg.input(output_t),
+                    ffmpeg.input(vtt),
+                    output,
+                    **{'c': 'copy', 'c:s': 'mov_text'},
+                ).run()
+                os.remove(vtt)
+                os.remove(output_t)
+            else:
+                os.rename(output_t,output)
             logger.info(f"downloaded: {filename}")
         except:
             logger.error(traceback.format_exc())
